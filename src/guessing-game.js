43610d8743930a26c3ad179b3912b1675713dccc
Evgeniy Zakrevskiy <zakrevskii3112@gmail.com>
2019-10-06 14:11:37 +0300

    feat: add guess function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this._max = max;
+        this._min = min;
     }
 
     guess() {
-
+        return this._min + Math.ceil((this._max-this._min)/2)
     }
 
     lower() {
-
+        this._max = this.guess();
     }
 
     greater() {
-
+        this._min = this.guess();
     }
 }
 
